0920bfa275c84f7d5347498e920d938e45c0e0c5
José Ronaldo <dev.joseronaldo@gmail.com>
2022-08-21 18:07:02 -0300

    :recycle: refactor: Componentizando a aplicação

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,73 @@
-export function SideBar() {
-  // Complete aqui
+import React, { useEffect, useState } from "react";
+import { api } from "../services/api";
+import { Button } from "./Button";
+
+import '../styles/sidebar.scss'
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface SidebarProps {
+  setSelectedGenre: React.Dispatch<React.SetStateAction<GenreResponseProps>>
+  setMovies: React.Dispatch<React.SetStateAction<MovieProps[]>>
+}
+
+export function SideBar({ setSelectedGenre, setMovies }: SidebarProps) {
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+  const [selectedGenreId, setSelectedGenreId] = useState(1);
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then(response => {
+      setGenres(response.data);
+    });
+  }, []);
+
+    //sidebar
+    useEffect(() => {
+      api.get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`).then(response => {
+        setMovies(response.data);
+      });
+  
+      api.get<GenreResponseProps>(`genres/${selectedGenreId}`).then(response => {
+        setSelectedGenre(response.data);
+      })
+    }, [selectedGenreId]);
+  
+    //sidebar
+    function handleClickButton(id: number) {
+      setSelectedGenreId(id);
+    }
+  
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  )
 }
